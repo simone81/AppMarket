546f1a5aea80b9fc4bef48ecd547e65642353ab2
jiapq <jiapq@behoo.net>
2011-01-09 17:43:00 +0800

    add uis to display downloading procedure

--- src/net/behoo/appmarket/AppDownloadPage.java
+++ src/net/behoo/appmarket/AppDownloadPage.java
@@ -17,16 +17,17 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.ServiceConnection;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.text.format.Formatter;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.ResourceCursorAdapter;
 import android.widget.TextView;
 import android.widget.AdapterView.OnItemSelectedListener;
@@ -35,35 +36,37 @@ import android.provider.Downloads;
 public class AppDownloadPage extends AsyncTaskActivity implements OnItemSelectedListener {
 	private static final String TAG = "AppDownloadPage";
 	
+	private ListAdapter mListApater = null;
 	private ListView mListView = null;
 	private PackageDbHelper mPkgDBHelper = null;
 	private InstallButtonGuard mInstallButtonGuard = null;
 	private Button mInstallButton = null;
 	private ImageView mAppImage = null;
-
-	private Map<String, AppInfo> mAppMap = new HashMap<String, AppInfo>();
-	private DownloadInstallService mInstallService = null;
+	private ProgressBar mDownloadProgressBar = null;
+	
+	private Map<String, AppInfo> mCodeAppInfoMap = new HashMap<String, AppInfo>();
 	private Cursor mDownloadCursor = null;
 	
-	private ServiceConnection mServiceConn = new ServiceConnection() {
-    	
-    	public void onServiceConnected(ComponentName cname, IBinder binder){
-    		mInstallService = ((DownloadInstallService.LocalServiceBinder)binder).getService();
-    		
-    		initList();
-    		createInstallButtonGuard();
-    	}
-    	
-    	public void onServiceDisconnected(ComponentName cname){
-    		mInstallService = null;
-    	}
-    };
-    
     private BroadcastReceiver mReceiver = new BroadcastReceiver() {
 		public void onReceive(Context context, Intent intent) {
-			Log.i(TAG, "onReceive");
+			Log.i(TAG, "onReceive application state changed");
 			if (null != mInstallButtonGuard) {
 				mInstallButtonGuard.updateAppState();
+			}
+			
+			Bundle bundle = intent.getExtras();
+			String code = bundle.getString(Constants.PACKAGE_CODE);
+			String state = bundle.getString(Constants.PACKAGE_STATE);
+			// update the progress bar
+			switch (Constants.PackageState.valueOf(state)) {
+			case installing:
+			case install_failed:
+			case install_succeeded:
+			case need_update:
+				mListApater.notifyDataSetChanged();
+				break;
+			default:
+				break;
 			}
 		}
 	};
@@ -81,18 +84,20 @@ public class AppDownloadPage extends AsyncTaskActivity implements OnItemSelected
 		
 		mInstallButton = (Button)findViewById(R.id.downloadpage_btn_to_install);
 		mAppImage = (ImageView)findViewById(R.id.main_app_logo);
+		mDownloadProgressBar = (ProgressBar)findViewById(R.id.downloadpage_progress);
+		
+		initList();
+		createInstallButtonGuard();
 	}
 	
 	public void onResume() {
     	super.onResume();
-    	bindService(new Intent(this, DownloadInstallService.class), mServiceConn, Context.BIND_AUTO_CREATE);
     	registerReceiver(mReceiver, new IntentFilter(Constants.ACTION_DWONLOAD_INSTALL_STATE));
     }
     
     public void onPause() {
     	super.onPause();
-    	unbindService( mServiceConn );
-    	unregisterReceiver( mReceiver );
+    	unregisterReceiver(mReceiver);
     }
     
 	public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -109,7 +114,7 @@ public class AppDownloadPage extends AsyncTaskActivity implements OnItemSelected
 		if (ListView.INVALID_POSITION != pos) {
 			String code = (String)mListView.getItemAtPosition(pos);
 			if (null != code) {
-				AppInfo appInfo = mAppMap.get(code);
+				AppInfo appInfo = mCodeAppInfoMap.get(code);
 				
 				TextView tv = (TextView)findViewById(R.id.main_app_title);
 				tv.setText(appInfo.mAppName);
@@ -130,7 +135,7 @@ public class AppDownloadPage extends AsyncTaskActivity implements OnItemSelected
 	
 	protected void onImageCompleted(boolean result, String url, String appcode) {
 		if (result) {
-			updateImage(mAppMap.get(appcode));
+			updateImage(mCodeAppInfoMap.get(appcode));
         } 
 	}
 	
@@ -150,7 +155,8 @@ public class AppDownloadPage extends AsyncTaskActivity implements OnItemSelected
 	
 	private void initList() {
 		mListView = (ListView)findViewById(R.id.downloadpage_list);
-		mListView.setAdapter(new ListAdapter(this, android.R.layout.simple_list_item_1, mDownloadCursor));
+		mListApater = new ListAdapter(this, android.R.layout.simple_list_item_1, mDownloadCursor);
+		mListView.setAdapter(mListApater);
 		mListView.setOnItemSelectedListener(this);
 		mListView.requestFocus();
 	}
@@ -159,26 +165,79 @@ public class AppDownloadPage extends AsyncTaskActivity implements OnItemSelected
 		int pos = mListView.getSelectedItemPosition();
 		if (ListView.INVALID_POSITION != pos) {
 			String code = (String)mListView.getItemAtPosition(pos);
-			if (null != code) {
-				if (null == mInstallButtonGuard) {
-					mInstallButtonGuard = new InstallButtonGuard(mInstallButton, 
-							mAppMap.get(code), mInstallService);
-				}
-				else {
-					mInstallButtonGuard.setAppInfo(mAppMap.get(code));
-				}
+			if (null == mInstallButtonGuard) {
+				mInstallButtonGuard = new InstallButtonGuard(mInstallButton, 
+						mCodeAppInfoMap.get(code), ServiceManager.inst().getDownloadHandler());
+			}
+			else {
+				mInstallButtonGuard.setAppInfo(mCodeAppInfoMap.get(code));
 			}
 		}		
 	}
 	
 	private void addAppInfo(String code) {
-		if (!mAppMap.containsKey(code)) {
+		if (!mCodeAppInfoMap.containsKey(code)) {
     		String where = PackageDbHelper.COLUMN_CODE + "=?";
     		String [] whereArgs = {code};
-    		ArrayList<AppInfo> appList = mInstallService.getAppList(where, whereArgs);
+    		ArrayList<AppInfo> appList = ServiceManager.inst().getDownloadHandler().getAppList(where, whereArgs);
     		Assert.assertTrue(null != appList && appList.size() == 1);
-    		mAppMap.put(code, appList.get(0));
+    		mCodeAppInfoMap.put(code, appList.get(0));
     	}
+	}
+	
+	private void updateAppStatesUIs(View view, String code,
+			boolean completed, long currentBytes, long totalBytes) {
+
+		boolean bIsSelected = false;
+		int pos = mListView.getSelectedItemPosition();
+		if (ListView.INVALID_POSITION != pos) {
+			String selCode = (String)mListView.getItemAtPosition(pos);
+			bIsSelected = (0 == selCode.compareTo(code));
+		}
+		else {
+			mDownloadProgressBar.setVisibility(View.INVISIBLE);
+		}
+		
+		AppInfo appInfo = mCodeAppInfoMap.get(code);
+		StringBuilder sb = new StringBuilder();
+		sb.append(appInfo.mAppName);
+		sb.append(' ');
+		
+		TextView tv = (TextView)view;
+		// update the item view of the list view
+		if (completed) {
+			// display string according to the state of the local database
+			Constants.PackageState state = 
+				ServiceManager.inst().getDownloadHandler().getAppState(code);
+			sb.append(state.name());
+			tv.setText(sb.toString());
+			
+			if (bIsSelected) {
+				mDownloadProgressBar.setVisibility(View.INVISIBLE);
+			}
+		}
+		else if (0 < totalBytes) {
+			// 10% for installation
+			long progress = currentBytes*90/totalBytes;
+			sb.append(progress);
+			sb.append('%');
+        	tv.setText(sb.toString());
+        	
+        	if (bIsSelected) {
+        		mDownloadProgressBar.setVisibility(View.VISIBLE);
+        		mDownloadProgressBar.setIndeterminate(false);
+        		mDownloadProgressBar.setProgress((int)progress);
+        	}
+		}
+		else {
+			Log.w(TAG, "bindView: the apk's length is not known!");
+			sb.append(getString(R.string.downloadpage_beginning));
+			tv.setText(sb.toString());
+			if (bIsSelected) {
+        		mDownloadProgressBar.setVisibility(View.VISIBLE);
+        		mDownloadProgressBar.setIndeterminate(true);
+        	}
+		}
 	}
 	
 	private class ListAdapter extends ResourceCursorAdapter {   
@@ -194,6 +253,7 @@ public class AppDownloadPage extends AsyncTaskActivity implements OnItemSelected
         	super(context, layout, c);
              
         	mRowId = c.getColumnIndexOrThrow("_id");
+        	mStatusId = c.getColumnIndexOrThrow(Downloads.COLUMN_STATUS);
         	mDescId = c.getColumnIndexOrThrow(Downloads.COLUMN_DESCRIPTION);
             mTotalBytesId = c.getColumnIndexOrThrow(Downloads.COLUMN_TOTAL_BYTES);
             mCurBytesId = c.getColumnIndexOrThrow(Downloads.COLUMN_CURRENT_BYTES);
@@ -202,30 +262,26 @@ public class AppDownloadPage extends AsyncTaskActivity implements OnItemSelected
         }
         
         public Object getItem(int position) {
-        	int iIndex = mCursor.getPosition();
         	if (mCursor.moveToPosition(position)) {
-        		String code = mCursor.getString(mDescId);
-        		// can't go back to -1 :(
-        		if (-1 != iIndex) {
-        			mCursor.moveToPosition(iIndex);
-        		}
-        		AppDownloadPage.this.addAppInfo(code);
-        		return code;
+        		return mCursor.getString(mDescId);
         	}
         	return null;
         }
         
         public void bindView(View view, Context context, Cursor cursor) {
-        	String str = cursor.getString(mDescId);
-        	str += "--";
-        	str += cursor.getString(mCurBytesId);
-        	str += "--";
-        	str += cursor.getString(mTotalBytesId);
+        	String code = cursor.getString(mDescId);
+        	addAppInfo(code);
         	
-        	TextView tv = (TextView)view;
-        	tv.setText(str);
-        	
-        	AppDownloadPage.this.addAppInfo(cursor.getString(mDescId));
+    		int status = cursor.getInt(mStatusId);
+    		if (Downloads.isStatusCompleted(status)) {
+    			// hide the progress bar, update the 
+    			updateAppStatesUIs(view, code, true, 0, 0);
+    		}
+    		else {
+    			long totalBytes = cursor.getLong(mTotalBytesId);
+        		long currentBytes = cursor.getLong(mCurBytesId); 
+    			updateAppStatesUIs(view, code, false, currentBytes, totalBytes);		
+    		}
         }
     }
 }
